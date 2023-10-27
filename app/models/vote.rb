f8d317d9c9591c9d931cca89ff71b4be9f8e42f1
keiichirotakahashi <k16takahashi125@gmail.com>
2023-10-27 23:09:15 +0900

    create Vote model

---
+++ app/models/vote.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: votes
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  link_id    :bigint
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_votes_on_link_id  (link_id)
+#  index_votes_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (link_id => links.id)
+#  fk_rails_...  (user_id => users.id)
+#
+class Vote < ApplicationRecord
+  belongs_to :link, validate: true
+  belongs_to :user, validate: true
+end
